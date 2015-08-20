e684b065ce2678cb23b4abf7a07e8e231ae8c66f
Savinov Kirill <kirill@savinov.me>
2015-08-20 11:29:15 +0300

    Add i18n support

--- UsersCube.php
+++ UsersCube.php
@@ -3,19 +3,42 @@ namespace yii\userscube;
 
 use Yii;
 use yii\base\BootstrapInterface;
+use yii\base\InvalidConfigException;
+use yii\db\ActiveRecord;
 
-class UsersCube extends \yii\base\Module implements BootstrapInterface
+/**
+ * @version 0.0.1-prealpha
+ */
+class UsersCube extends \yii\base\Module //implements BootstrapInterface
 {
+
+    /** @const VERSION Module version */
     const VERSION = "0.0.1-prealpha";
 
+    /** @var string Alias for module */
+    public $alias = "@usercube";
+
+    /** @inheritdoc */
     public function init()
     {
         parent::init();
+
+        // set up i8n
+        if (empty(Yii::$app->i18n->translations['usercube'])) {
+            Yii::$app->i18n->translations['usercube'] = [
+                'class' => 'yii\i18n\PhpMessageSource',
+                'basePath' => __DIR__ . '/messages',
+            ];
+        }
+
+        $this->setAliases([
+            $this->alias => __DIR__,
+        ]);
+
     }
 
-    public function bootstrap($app)
+/*    public function bootstrap($app)
     {
         Yii::setAlias('userscube', '@vendor/cubiclab/users-cube');
-
-    }
+    }*/
 }
---
+++ messages/ru/usercubic.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pt1c
+ * Date: 20.08.2015
+ * Time: 11:07
+ */
+
+return [
+    'ID'            => '№',
+    'Username'      => 'Логин пользователя',
+    'Password'      => 'Пароль',
+    'Email'         => 'E-mail адрес',
+    'New Email'     => 'Новый E-mail адрес',
+    'Auth Key'      => 'Ключ авторизации',
+    'Api Key'       => 'Ключ API',
+    'Status'        => 'Статус',
+    'Login Ip'      => 'IP авторизации',
+    'Login Time'    => 'Дата авторизации',
+    'Created At'    => 'Дата авторизации',
+    'Updated At'    => 'Дата авторизации',
+    'Created By'    => 'Дата авторизации',
+    'Updated By'    => 'Дата авторизации',
+
+    'Current Password'        => 'Настоящий пароль',
+    'New Password'            => 'Новый пароль',
+    'New Password Confirm'    => 'Повтор нового пароля',
+
+    'Passwords do not match' => 'Пароли не совпадают',
+    '{attribute} can contain only letters, numbers, and "_"' => '{attribute} может содержать только буквы, цифры и "_"',
+];
