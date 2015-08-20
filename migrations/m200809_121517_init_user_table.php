7135b220d121986329b0053c82acd732a9253276
Savinov Kirill <kirill@savinov.me>
2015-08-20 11:28:22 +0300

    Add user table (1 variant)

---
+++ migrations/m200809_121517_init_user_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m200809_121517_init_user_table extends Migration
+{
+    public function up()
+    {
+        $this->createTable(
+            'user',
+            [
+                'id' => 'pk',
+                'username' => 'string UNIQUE',
+                'password' => 'string',
+                'auth_key' => 'string UNIQUE',
+            ]
+        );
+
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%user}}', [
+            'id'            => Schema::TYPE_PK,
+            'username'      => Schema::TYPE_STRING . ' null default null',
+            'password'      => Schema::TYPE_STRING . ' null default null',
+            'email'         => Schema::TYPE_STRING . ' null default null',
+            'auth_key'      => Schema::TYPE_STRING . ' null default null',
+            'api_key'       => Schema::TYPE_STRING . ' null default null',
+            'status'        => Schema::TYPE_SMALLINT . ' not null',
+            'login_ip'      => Schema::TYPE_STRING . ' null default null',
+            'login_time'    => Schema::TYPE_TIMESTAMP . ' null default null',
+            'created_at'    => Schema::TYPE_TIMESTAMP . ' null default null',
+            'updated_at'    => Schema::TYPE_TIMESTAMP . ' null default null',
+            'created_by'    => Schema::TYPE_INTEGER . ' null default null',
+            'updated_by'    => Schema::TYPE_INTEGER . ' null default null',
+        ], $tableOptions);
+
+
+    }
+
+    public function down()
+    {
+        echo "m150809_121517_init_user_table cannot be reverted.\n";
+
+        $this->dropTable('{{%user}}');
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
